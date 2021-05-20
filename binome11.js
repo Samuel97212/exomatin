0e4217c2f952eac9771ff18a9f13e596463c2093
Samuel Poulade <poulade.samuel@gmail.com>
2021-05-20 12:19:00 +0200

    Algo2005

---
+++ binome11.js
@@ -0,0 +1,55 @@
+// Exercice I
+var tableau = [
+    {
+      id: 0,
+      product: 'shoes',
+      price: 70
+    },
+    {
+      id: 1,
+      product: 'screen',
+      price: 90
+    },
+    {
+      id: 2,
+      product: 'playstation V',
+      price: 499
+    },
+    {
+      id: 3,
+      product: 'jean',
+      price: 50
+    }
+  ]
+
+
+// var calcul = 0
+// for (var i = 0; i < tableau.length; i++) {
+// calcul += tableau[i].price
+// }
+// console.log(calcul)
+
+// EXO 2 
+
+// var laPlusPetitVal = 0
+
+// function par() {
+// for(var i = 0 ; i< tableau.length; i++) {
+//     if(tableau[0].price < tableau[i].price) { 
+//         laPlusPetitVal = tableau[0].price
+//   } else { 
+//       laPlusPetitVal = tableau[i].price
+//   }
+// }
+// console.log(laPlusPetitVal)
+// }
+// par()
+
+// EXO 3
+
+// function ajout (s) {
+//     tableau.push(s)
+//     console.log(tableau)
+// }
+
+// ajout("stock")
